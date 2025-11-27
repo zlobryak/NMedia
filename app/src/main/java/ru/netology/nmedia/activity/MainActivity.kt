2b38b279ee124fcfcb8424d727c34f8c72b68882
Alexander Melnichenko <clychnik@gmail.com>
2025-11-27 14:41:23 +0300

    RecyclerView integration in process

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.databinding.CardPostBinding
 import ru.netology.nmedia.functions.counterFormatter
 import ru.netology.nmedia.viewmodel.PostViewModel
 
@@ -20,30 +22,41 @@ class MainActivity : AppCompatActivity() {
         val margin = resources.getDimensionPixelSize(R.dimen.common_spacing)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left + margin, systemBars.top + margin, systemBars.right + margin, systemBars.bottom + margin)
+            v.setPadding(
+                systemBars.left + margin,
+                systemBars.top + margin,
+                systemBars.right + margin,
+                systemBars.bottom + margin
+            )
             insets
         }
 
         val viewModel: PostViewModel by viewModels()
-        viewModel.data.observe(this) { post ->
-            with(binding) {
-                avatar.setImageResource(R.drawable.ic_netology_48dp)
-                author.text = post.author
-                content.text = post.content
-                published.text = post.published
-                likesCount.text = counterFormatter(post.likes)
-                shareCount.text = counterFormatter(post.shareCount)
-                if (post.likedByMe) {
-                    icLikes.setImageResource(R.drawable.ic_liked_24)
-                } else(icLikes.setImageResource((R.drawable.ic_like_24)))
+        viewModel.data.observe(this) { posts ->
+            posts.map { post ->
+                CardPostBinding.inflate(layoutInflater, binding.root, true).apply {
+                    avatar.setImageResource(R.drawable.ic_netology_48dp)
+                    author.text = post.author
+                    content.text = post.content
+                    published.text = post.published
+                    likesCount.text = counterFormatter(post.likes)
+                    shareCount.text = counterFormatter(post.shareCount)
+                    if (post.likedByMe) {
+                        icLikes.setImageResource(R.drawable.ic_liked_24)
+                    } else (icLikes.setImageResource((R.drawable.ic_like_24)))
+                }
+
+
+                icLikes.setOnClickListener {
+                    viewModel.likeById(post.id)
+                }
+                icShare.setOnClickListener {
+                    viewModel.shareById(post.id)
+
             }
 
-        }
-        binding.icLikes.setOnClickListener {
-            viewModel.like()
-        }
-        binding.icShare.setOnClickListener {
-            viewModel.share()
+
+            }
         }
     }
 }
