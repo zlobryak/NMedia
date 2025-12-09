b20e7e7b27dc6829dafd6de4ba0339c79eeb401c
Alexander Melnichenko <clychnik@gmail.com>
2025-12-10 02:01:45 +0300

    Video preview View added

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,25 +1,25 @@
 package ru.netology.nmedia.viewmodel
 
-import androidx.lifecycle.MutableLiveData
+//import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
 
-private val empty = Post(
-    id = 0,
-    author = "",
-    content = "",
-    published = "",
-    likes = 0,
-    shareCount = 0,
-    likedByMe = false
-)
+//private val empty = Post(
+//    id = 0,
+//    author = "",
+//    content = "",
+//    published = "",
+//    likes = 0,
+//    shareCount = 0,
+//    likedByMe = false
+//)
 
 class PostViewModel : ViewModel() {
     private val repository: PostRepository = PostRepositoryInMemoryImpl()
     val data = repository.getAll()
-    val edited = MutableLiveData(empty)
+//    val edited = MutableLiveData(empty)
 
     fun likeById(id: Long) = repository.likeById(id)
     fun shareById(id: Long) = repository.shareById(id)
