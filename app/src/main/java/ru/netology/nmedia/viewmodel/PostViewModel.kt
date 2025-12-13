30fff28d32e0d6f45120cdfeba582e681b276d21
Alexander Melnichenko <clychnik@gmail.com>
2025-12-13 20:42:57 +0300

    In memory file implementation

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,39 +1,22 @@
 package ru.netology.nmedia.viewmodel
 
 //import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
-import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
+import ru.netology.nmedia.repository.PostRepositoryInFileImpl
 
-//private val empty = Post(
-//    id = 0,
-//    author = "",
-//    content = "",
-//    published = "",
-//    likes = 0,
-//    shareCount = 0,
-//    likedByMe = false
-//)
 
-class PostViewModel : ViewModel() {
-    private val repository: PostRepository = PostRepositoryInMemoryImpl()
+class PostViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: PostRepository = PostRepositoryInFileImpl(application)
     val data = repository.getAll()
 //    val edited = MutableLiveData(empty)
 
     fun likeById(id: Long) = repository.likeById(id)
     fun shareById(id: Long) = repository.shareById(id)
-    fun removeById(id: Long) = repository.removeBbyId(id)
+    fun removeById(id: Long) = repository.removeById(id)
     fun save(post: Post) {
         repository.save(post)
-//        edited.value = empty
     }
-
-//    fun edit(post: Post) {
-//        edited.value = post
-//    }
-//
-//    fun resetEdited() {
-//        edited.value = empty
-//    }
 }
