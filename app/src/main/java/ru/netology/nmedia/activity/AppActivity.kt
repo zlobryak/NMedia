99492e6a443aa72250a9fddd7495f0ce07da424b
Alexander Melnichenko <clychnik@gmail.com>
2026-01-31 23:04:59 +0300

    Simple Post added

--- app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
@@ -6,10 +6,35 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.findNavController
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
@@ -32,14 +57,35 @@ class AppActivity : AppCompatActivity() {
 
         // Инициализация binding'а для макета activity_app
         val binding = ActivityAppBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        // Настройка отступов для корневого View с учётом системных баров (статус-бар, навигационная панель)
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+//Ниже старая версия, где быо=л XML вместо JetPack Compose
+////        setContentView(binding.root)
+//
+//        // Настройка отступов для корневого View с учётом системных баров (статус-бар, навигационная панель)
+//        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        setContent {
+            MaterialTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PostCard(
+                        Post(
+                            author = "Sergey",
+                            published = "31.01.2025",
+                            content = "\uD83D\uDD25 Тест прошёл. Система в норме.\n" +
+                                    "Готов к работе.",
+                            likedByMe = false
+                        ),
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
         }
+
+
         requestNotificationsPermission()
 
         // Получаем действие, с которым был запущен Intent
@@ -99,12 +145,102 @@ class AppActivity : AppCompatActivity() {
                 getErrorDialog(this@AppActivity, code, 9000)?.show()
                 return
             }
-            Toast.makeText(this@AppActivity,
-                getString(R.string.google_play_unavailable), Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                this@AppActivity,
+                getString(R.string.google_play_unavailable), Toast.LENGTH_LONG
+            ).show()
         }
 
         FirebaseMessaging.getInstance().token.addOnSuccessListener {
             println(it)
         }
+    }
+}
+
+//Ниже закомментирована тестовая функция для Jetpack Compose
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name",
+//        modifier = modifier
+//    )
+//
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    Greeting("Test")
+//}
+
+data class Post(
+    val author: String,
+    val published: String,
+    val content: String,
+    val likedByMe: Boolean
+)
+
+@Composable
+fun PostCard(post: Post, modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .fillMaxHeight()
+            .padding(8.dp)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Box(
+                modifier = Modifier
+                    .size(48.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.primary),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = post.author.take(1))
+            }
+            Spacer(Modifier.width(8.dp))
+            Column {
+                Text(post.author)
+                Text(post.published)
+            }
+        }
+
+        Spacer(Modifier.height(8.dp))
+
+        Text(post.content)
+//TODO Уменьшить отступ иконки
+        IconButton(onClick = {}, Modifier.padding(start = (-12).dp)) {
+            Icon(
+                imageVector = if (post.likedByMe) {
+                    Icons.Filled.Favorite
+                } else {
+                    Icons.Outlined.FavoriteBorder
+                },
+                contentDescription = if (post.likedByMe) {
+                    "Unlike"
+                } else {
+                    "Like"
+                },
+                tint = if (post.likedByMe) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.size(24.dp)
+            )
+        }
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PostCardPreview() {
+    MaterialTheme {
+        PostCard(
+            Post(
+                author = "Sergey",
+                published = "31.01.2025",
+                content = "\uD83D\uDD25 Тест прошёл. Система в норме.\n" +
+                        "Готов к работе.",
+                likedByMe = true
+            )
+        )
     }
 }
